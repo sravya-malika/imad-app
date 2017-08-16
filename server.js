1d9483c4096da3a86edb08c33c4271c2a16543db
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2017-08-16 18:39:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one', function (req, res) {
+  res.send('article one requested and served');
+});
+app.get('/article-two', function (req, res) {
+  res.send('article two requested and served');
+});
+app.get('/article-three', function (req, res) {
+  res.send('article three requested and served');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
