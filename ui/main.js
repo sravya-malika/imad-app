480c0bcd2d7a1d7c20f35eb71499267ebc21a0bf
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-02-17 14:37:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHTML='New Value';
+
+//move image
+var img=document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+};
