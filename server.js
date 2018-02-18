219710cb3aaecc685c944b2c9884281e865c7e43
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-02-18 15:51:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,11 +62,11 @@ var htmlTemplate = `
 `;
 return htmlTemplate;
 }
-//var counter=0;
-//app.get('/counter', function (req, res) {
-  //counter=counter+1;
-  //res.send(path.join(counter.toString()));
-//});
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(path.join(counter.toString()));
+});
 
 
 
