0bc3decc42c3fa7eff991ecde303321c12c951a1
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-02-18 17:00:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,17 +43,35 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a requst to server
+     var request= new XMLHttpRequest();
     
-    //capture and list
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0;i<names.length;i++)
-    {
-        list += '<li>'+names[i]+'</li>';   
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML=list;
+    //capture
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          //take action
+          if(request.status === 200)
+          {
+    
+                //capture and list
+                var names = request.responseText;
+                names= JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++)
+                {
+                    list += '<li>'+names[i]+'</li>';   
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML=list;
+          }
+      }
 };
+
+request.open('GET','http://ksravyamalika.imad.hasura-app.io/submit-name?name='+name,true);
+   request.send(null);
+};
+
+
 
 
 
