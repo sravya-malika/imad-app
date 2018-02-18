e8baf1bc9f26cbec05fb2e0dcc4a5198fe514812
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-02-18 19:53:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -74,7 +74,7 @@ submit.onclick = function(){
           }
       }
 };
-var nameInput =document.getElmentById('name');
+var nameInput =document.getElementById('name');
 var name = nameInput.value;
 request.open('GET','http://ksravyamalika.imad.hasura-app.io/submit-name?name='+name,true);
    request.send(null);
