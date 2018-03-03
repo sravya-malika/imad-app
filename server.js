047aff7f4fe71ee0c793c479b6cc1c3ed7102cdd
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-03-03 23:23:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/:articleNames', function (req, res) {
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    res.send(createTemplate(articles[articleNames]));
 
     
 });
