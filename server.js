58df05999d75798a3826ed37539aa2aa33ee3f02
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-03-04 13:03:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,11 +77,11 @@ var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
-            res.status(500).send(err.toString());
+            result.status(500).send(err.toString());
     
         }
         else{
-            res.send(JSON.stringify(result));
+            result.send(JSON.stringify(result));
         }
     });
 });
