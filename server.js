cb98c872b20ec4745730bd8b4fa0c6826adea08f
sravya-malika <30817458+sravya-malika@users.noreply.github.com>
2018-03-18 10:59:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ return htmlTemplate;
 function hash(input,salt){
      
      var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-     return hashed.toString('hex');
+     return [hashed.toString('hex')].join('$');
 }
 
 
@@ -93,11 +93,13 @@ app.get('/hash/:input',function(req,res){
 });
 
 app.post('/create-user',function(req,res){
+    
     var username = req.body.username;
     var password = req.body.password;
     
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password,salt);
+    
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString], function(err, result){
         if(err){
             res.status(500).send(err.toString());
